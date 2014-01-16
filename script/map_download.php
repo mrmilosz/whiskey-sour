511c37d8e7f8737ff2d53d495d35a6807a36b902
mrmilosz <milosz@milosz.ca>
2014-01-16 05:16:45 +0400

    Fixed a real dumb bug

--- script/map_download.php
+++ script/map_download.php
@@ -1,7 +1,7 @@
 <?php
 $config = json_decode(file_get_contents('../server.json'), true);
 
-$game_path = "${config['q3a_path']}${config['mod']}";
+$game_path = "${config['q3a_path']}${config['mod']}/";
 $pk3_destination_path_format = "${config['pk3_directory']}/%s";
 $curl_user_agent_string = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13';
 $rcon_command = 'reload_fs';
