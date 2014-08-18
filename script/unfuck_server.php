09399cd3f4af6bbb0e2f80021164bc90321a1940
mrmilosz <milosz@milosz.ca>
2014-08-18 12:54:57 -0400

    Weak attempt at improving unfuck button... rcon doesn't run without a map anyway!

--- script/unfuck_server.php
+++ script/unfuck_server.php
@@ -6,23 +6,40 @@ $rcon_response = 'Server is not running.';
 
 $output = array();
 
-# Check if the server is fucked
 $rcon_socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-$rcon_datagram = "\xff\xff\xff\xffrcon \"$rcon_password\" status"; 
-socket_sendto($rcon_socket, $rcon_datagram, strlen($rcon_datagram), 0, $_SERVER['SERVER_ADDR'], $config['quake_port']);
 
-# If it is not fucked, set an error
-if (socket_read($rcon_socket, strlen($rcon_response)) !== $rcon_response) {
-	$output['error'] = 'Server does not appear fucked though…';
+# First rcon for the status
+$rcon_datagram = "\xff\xff\xff\xffrcon \"$rcon_password\" status"; 
+if (socket_sendto($rcon_socket, $rcon_datagram, strlen($rcon_datagram), 0, $_SERVER['SERVER_ADDR'], $config['quake_port']) === FALSE) {
+	$output['error'] = 'Unfucking failed due to bad socket write on check. Contact the administrator.';
+	socket_close($rcon_socket);
+	finish($output);
+}
+
+# Read the status from the socket
+$rcon_buffer = '';
+if (socket_recv($rcon_socket, $rcon_buffer, strlen($rcon_response), 0, $_SERVER['SERVER_ADDR'], $config['quake_port']) ===  FALSE) {
+	$output['error'] = 'Unfucking failed due to bad socket read on check. Contact the administrator.';
+	socket_close($rcon_socket);
+	finish($output);
+}
+
+# If the server is not fucked, set an error
+if ($rcon_buffer !== $rcon_response) {
+	$output['error'] = "Server says: \"$rcon_buffer\"";
 	socket_close($rcon_socket);
 	finish($output);
 }
 
 # If it is fucked, fix it
-$rcon_datagram = "\xff\xff\xff\xffrcon \"$rcon_password\" exec cfgs/idle.cfg"; 
-socket_sendto($rcon_socket, $rcon_datagram, strlen($rcon_datagram), 0, $_SERVER['SERVER_ADDR'], $config['quake_port']);
-socket_close($rcon_socket);
+$rcon_datagram = "\xff\xff\xff\xffrcon \"$rcon_password\" exec {$config['quake_idle_cfg']}"; 
+if (socket_sendto($rcon_socket, $rcon_datagram, strlen($rcon_datagram), 0, $_SERVER['SERVER_ADDR'], $config['quake_port']) === FALSE) {
+	$output['error'] = 'Unfucking failed due to bad socket write on unfuck. Contact the administrator.';
+	socket_close($rcon_socket);
+	finish($output);
+}
 
+socket_close($rcon_socket);
 $output['success'] = "Sent unfucking command to server.";
 finish($output);
 function finish($output) {
